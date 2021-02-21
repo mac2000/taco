bb161136887d8d9bbcdde00e3f41efe761fa7170
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-02-21 15:43:15 +0200

    basic layout

--- src/main/java/com/example/taco/views/HomePageView.java
+++ src/main/java/com/example/taco/views/HomePageView.java
@@ -2,6 +2,8 @@ package com.example.taco.views;
 
 import com.example.taco.annotations.View;
 
+import java.util.Arrays;
+
 import static j2html.TagCreator.*;
 
 @View
@@ -18,12 +20,81 @@ public class HomePageView implements SimpleView {
                                 link().withRel("stylesheet").withHref("/css/shop-homepage.css")
                         ),
                         body(
+                                nav().withClass("navbar navbar-expand-lg navbar-dark bg-dark fixed-top").with(
+                                        div().withClass("container").with(
+                                                a("Taco Factory").withClass("navbar-brand").withHref("/")
+                                        )
+                                ),
+
+                                div().withClass("container").with(
+                                        div().withClass("row").with(
+                                                div().withClass("col-lg-3").with(
+                                                        h1("Taco Factory").withClass("my-4"),
+                                                        div().withClass("list-group").with(
+                                                                a("Category 1").withClass("list-group-item").withHref("#"),
+                                                                a("Category 2").withClass("list-group-item").withHref("#"),
+                                                                a("Category 3").withClass("list-group-item").withHref("#")
+                                                        )
+                                                ),
+                                                div().withClass("col-lg-9").with(
+                                                        div().withId("carouselExampleIndicators").withClass("carousel slide my-4").attr("data-ride", "carousel").with(
+                                                                ol().withClass("carousel-indicators").with(
+                                                                        li().attr("data-target", "#carouselExampleIndicators").attr("data-slide-to", "0").withClass("active"),
+                                                                        li().attr("data-target", "#carouselExampleIndicators").attr("data-slide-to", "1"),
+                                                                        li().attr("data-target", "#carouselExampleIndicators").attr("data-slide-to", "2")
+                                                                ),
+                                                                div().withClass("carousel-inner").withRole("listbox").with(
+                                                                        div().withClass("carousel-item active").with(
+                                                                                img().withClass("d-block img-fluid").withAlt("First slide").withSrc("http://placehold.it/900x350")
+                                                                        ),
+                                                                        div().withClass("carousel-item").with(
+                                                                                img().withClass("d-block img-fluid").withAlt("Second slide").withSrc("http://placehold.it/900x350")
+                                                                        ),
+                                                                        div().withClass("carousel-item").with(
+                                                                                img().withClass("d-block img-fluid").withAlt("Third slide").withSrc("http://placehold.it/900x350")
+                                                                        )
+                                                                ),
+                                                                a().withClass("carousel-control-prev").attr("data-slide", "prev").withHref("#carouselExampleIndicators").withRole("button").with(
+                                                                        span().withClass("carousel-control-prev-icon").attr("aria-hidden", "true"),
+                                                                        span("Previous").withClass("sr-only")
+                                                                ),
+                                                                a().withClass("carousel-control-next").attr("data-slide", "next").withHref("#carouselExampleIndicators").withRole("button").with(
+                                                                        span().withClass("carousel-control-next-icon").attr("aria-hidden", "true"),
+                                                                        span("Next").withClass("sr-only")
+                                                                )
+                                                        ),
+                                                        div().withClass("row").with(
+                                                                each(Arrays.asList("Item One", "Item Two", "Item Three", "Item Four", "Item Five", "Item Six"), name -> div().withClass("col-lg-4 col-md-6 mb-4").with(
+                                                                        div().withClass("card h-100").with(
+                                                                                a().withHref("#").with(
+                                                                                        img().withClass("card-img-top").withAlt("").withSrc("http://placehold.it/700x400")
+                                                                                ),
+                                                                                div().withClass("card-body").with(
+                                                                                        h4().withClass("card-title").with(
+                                                                                                a(name).withHref("#")
+                                                                                        ),
+                                                                                        h5("$24.99"),
+                                                                                        p("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!").withClass("card-text")
+
+                                                                                ),
+                                                                                div().withClass("card-footer").with(
+                                                                                        small("⭐ ⭐ ⭐ ⭐ ⭐").withClass("text-muted")
+                                                                                )
+                                                                        )
+                                                                ))
+                                                        )
+                                                )
+                                        )
+                                ),
+
                                 h1("hello"),
+
                                 footer().withClass("py-5 bg-dark").with(
                                         div().withClass("container").with(
                                                 p("Copyright © Taco Factory 2021").withClass("m-0 text-center text-white")
                                         )
                                 ),
+
                                 script().withSrc("/vendor/jquery/jquery.min.js"),
                                 script().withSrc("/vendor/bootstrap/js/bootstrap.bundle.min.js")
                         )
--- src/test/java/com/example/taco/controllers/HomeControllerTest.java
+++ src/test/java/com/example/taco/controllers/HomeControllerTest.java
@@ -62,11 +62,60 @@ class HomeControllerTest {
     }
 
     @Test
+    void shouldHaveHeader() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark fixed-top\">")))
+                .andExpect(content().string(containsString("<a class=\"navbar-brand\" href=\"/\">Taco Factory</a>")))
+                .andExpect(content().string(containsString("</nav>")));
+    }
+
+    @Test
     void shouldHaveFooter() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("<footer class=\"py-5 bg-dark\">")))
                 .andExpect(content().string(containsString("Copyright © Taco Factory 2021")))
                 .andExpect(content().string(containsString("</footer>")));
+    }
+
+    @Test
+    void shouldHaveSeoHeader() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<h1 class=\"my-4\">")))
+                .andExpect(content().string(containsString("Taco Factory")))
+                .andExpect(content().string(containsString("</h1>")));
+    }
+
+    @Test
+    void shouldHaveDemoCategories() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<a class=\"list-group-item\" href=\"#\">Category 1</a>")))
+                .andExpect(content().string(containsString("<a class=\"list-group-item\" href=\"#\">Category 2</a>")))
+                .andExpect(content().string(containsString("<a class=\"list-group-item\" href=\"#\">Category 3</a>")));
+    }
+
+    @Test
+    void shouldHaveDemoItems() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<a href=\"#\">Item One</a>")))
+                .andExpect(content().string(containsString("<a href=\"#\">Item Two</a>")))
+                .andExpect(content().string(containsString("<a href=\"#\">Item Three</a>")))
+                .andExpect(content().string(containsString("<a href=\"#\">Item Four</a>")))
+                .andExpect(content().string(containsString("<a href=\"#\">Item Five</a>")))
+                .andExpect(content().string(containsString("<a href=\"#\">Item Six</a>")));
+    }
+
+    @Test
+    void shouldHaveDemoCarousel() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("id=\"carouselExampleIndicators\"")))
+                .andExpect(content().string(containsString("alt=\"First slide\"")))
+                .andExpect(content().string(containsString("alt=\"Second slide\"")))
+                .andExpect(content().string(containsString("alt=\"Third slide\"")));
     }
 }
