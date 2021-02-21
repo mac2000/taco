a2cc7be1d1adaf61e91f2dfbc5a6fbcc689d668e
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-02-21 16:16:50 +0200

    basic layout copy pasta

--- src/main/java/com/example/taco/views/DesignPageView.java
+++ src/main/java/com/example/taco/views/DesignPageView.java
@@ -11,7 +11,27 @@ public class DesignPageView implements SimpleView {
         return document(
                 html(
                         head(
-                                meta().withCharset("utf-8")
+                                meta().withCharset("utf-8"),
+                                meta().withName("viewport").withContent("width=device-width, initial-scale=1, shrink-to-fit=no"),
+                                title("Design - Taco Factory"),
+                                link().withRel("stylesheet").withHref("/vendor/bootstrap/css/bootstrap.min.css"),
+                                link().withRel("stylesheet").withHref("/css/shop-homepage.css")
+                        ),
+                        body(
+                                nav().withClass("navbar navbar-expand-lg navbar-dark bg-dark fixed-top").with(
+                                        div().withClass("container").with(
+                                                a("Taco Factory").withClass("navbar-brand").withHref("/")
+                                        )
+                                ),
+
+                                footer().withClass("py-5 bg-dark").with(
+                                        div().withClass("container").with(
+                                                p("Copyright © Taco Factory 2021").withClass("m-0 text-center text-white")
+                                        )
+                                ),
+
+                                script().withSrc("/vendor/jquery/jquery.min.js"),
+                                script().withSrc("/vendor/bootstrap/js/bootstrap.bundle.min.js")
                         )
                 )
         );
--- src/test/java/com/example/taco/controllers/DesignControllerTest.java
+++ src/test/java/com/example/taco/controllers/DesignControllerTest.java
@@ -30,4 +30,52 @@ class DesignControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("<meta charset=\"utf-8\">")));
     }
+
+    @Test
+    void shouldHaveMetaViewport() throws Exception {
+        mockMvc.perform(get(URL.DESIGN))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">")));
+    }
+
+    @Test
+    void shouldHaveHeadTitle() throws Exception {
+        mockMvc.perform(get(URL.DESIGN))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<title>Design - Taco Factory</title>")));
+    }
+
+    @Test
+    void shouldHaveStylesConnected() throws Exception {
+        mockMvc.perform(get(URL.DESIGN))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<link rel=\"stylesheet\" href=\"/vendor/bootstrap/css/bootstrap.min.css\">")))
+                .andExpect(content().string(containsString("<link rel=\"stylesheet\" href=\"/css/shop-homepage.css\">")));
+    }
+
+    @Test
+    void shouldHaveScriptsConnected() throws Exception {
+        mockMvc.perform(get(URL.DESIGN))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<script src=\"/vendor/jquery/jquery.min.js\"></script>")))
+                .andExpect(content().string(containsString("<script src=\"/vendor/bootstrap/js/bootstrap.bundle.min.js\"></script>")));
+    }
+
+    @Test
+    void shouldHaveHeader() throws Exception {
+        mockMvc.perform(get(URL.DESIGN))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<nav class=\"navbar navbar-expand-lg navbar-dark bg-dark fixed-top\">")))
+                .andExpect(content().string(containsString("<a class=\"navbar-brand\" href=\"/\">Taco Factory</a>")))
+                .andExpect(content().string(containsString("</nav>")));
+    }
+
+    @Test
+    void shouldHaveFooter() throws Exception {
+        mockMvc.perform(get(URL.DESIGN))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<footer class=\"py-5 bg-dark\">")))
+                .andExpect(content().string(containsString("Copyright © Taco Factory 2021")))
+                .andExpect(content().string(containsString("</footer>")));
+    }
 }
