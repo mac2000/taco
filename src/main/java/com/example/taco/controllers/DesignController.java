bb0dcad2889c4f9831fce0dcb8e55d7e50cc4c1c
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2021-02-21 15:53:03 +0200

    green

--- src/main/java/com/example/taco/controllers/DesignController.java
+++ src/main/java/com/example/taco/controllers/DesignController.java
@@ -1,8 +1,12 @@
 package com.example.taco.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DesignController {
-
+    @GetMapping("/design")
+    public String getDesignPage() {
+        return "";
+    }
 }
